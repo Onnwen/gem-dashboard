8662f94bb3b5d36687ea32ff93b76b5be6fe729e
Onnwen Cassitto <onnwen.cassitto@icloud.com>
2023-11-19 13:05:42 +0100

    Aggiunto call sizeSet per iframe

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Analytics } from '@vercel/analytics/react';
 import Nav from './nav';
 import Toast from './toast';
 import { Suspense } from 'react';
+import Safe from 'react-safe';
 
 export const metadata = {
   title: 'GEM',
@@ -19,13 +20,40 @@ export default async function RootLayout({
   return (
     // <html lang='it' className='h-full bg-gray-50'>
     <html lang='it'>
-    <body>
+    <body id={'wrapper'}>
     {/*<Suspense>*/}
     {/*  <Nav />*/}
     {/*</Suspense>*/}
     {children}
     {/*<Analytics />*/}
     {/*<Toast />*/}
+    <Safe.script>{
+      `
+      function sendHeight()
+      {
+        if(parent.postMessage)
+      {
+        var height= document.getElementById('wrapper').offsetHeight;
+        parent.postMessage(height, '*');
+      }
+      }
+
+      var eventMethod = window.addEventListener ? "addEventListener" : "attachEvent";
+      var eventer = window[eventMethod];
+      var messageEvent = eventMethod == "attachEvent" ? "onmessage" : "message";
+
+      eventer(messageEvent, function(e) {
+
+      if (isNaN(e.data)) return;
+
+      sendHeight();
+
+    },
+      false
+      )
+      ;`
+    }
+    </Safe.script>
     </body>
     </html>
   );
